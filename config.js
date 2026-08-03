628a350bfe002d57ee9594264b2e14fc42a884d0
joelmarinot2 <joelmarinot2@gmail.com>
2026-08-03 11:41:45 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -9,6 +9,6 @@
 //  los datos son las políticas RLS del esquema + el login.
 // ============================================================
 window.SUPA = {
-  url: "https://htapautuqgozroaauvpo.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0YXBhdXR1cWdvenJvYWF1dnBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1MzE0MjcsImV4cCI6MjA5OTEwNzQyN30.KpaOj0T6PnVmdEl80Ti_swI8TH3KKrRpeXqusd6colE"
+  url: "https://rdveoxcnrtirhxpmtmck.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkdmVveGNucnRpcmh4cG10bWNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3NjU4NzgsImV4cCI6MjEwMTM0MTg3OH0.Y1or0B7fm9_z1H36MGSkny-Po2_-sbZUyFG7GYDhH8g"
 };
