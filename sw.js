4c19b1b2e189397e0ee15dd1672ce58cdf489557
joelmarinot2 <jordimemesmomazosdick@gmail.com>
2026-08-28 21:50:05 -0500

    Version v10.15.0-nube (pausas por gesto en el texto + borrar pausas una a una); refresca cache del SW

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker · Dubbipt  (VERSION autogenerada en cada build)
-const VERSION = '2026-08-09T06:16';
+const VERSION = '2026-08-28T12:00';
 const CACHE   = 'dubbipt-' + VERSION;
 
 const SHELL = [
